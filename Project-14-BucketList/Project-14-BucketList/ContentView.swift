8a2f0ba3354e7457c61debe422c16a39baa882a1
Jonathan Miranda <miranda.jth@gmail.com>
2020-02-16 12:58:39 -0600

    next step MKMapView with SwiftUI

--- Project-14-BucketList/Project-14-BucketList/ContentView.swift
+++ Project-14-BucketList/Project-14-BucketList/ContentView.swift
@@ -6,42 +6,42 @@
 //  Copyright © 2020 Jonathan Miranda. All rights reserved.
 //
 
-import LocalAuthentication
+import MapKit
 import SwiftUI
 
 
 struct ContentView: View {
-    @State private var isUnlock = false
+    @State private var centerCoordinate = CLLocationCoordinate2D()
+    @State private var locations = [MKPointAnnotation]()
    
     var body: some View {
-        VStack {
-            if self.isUnlock {
-                Text("Unlock")
-            } else {
-                Text("Locked")
-            }
-        }
-        .onAppear(perform: authenticate)
-    }
-    
-    func authenticate() {
-        let context = LAContext()
-        var error: NSError?
-        
-        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
-            let reason = "We need to unlock your data."
+        ZStack {
+            MapView(centerCoordinate: $centerCoordinate, annotations: locations)
+                .edgesIgnoringSafeArea(.all)
+            Circle()
+                .fill(Color.blue)
+                .opacity(0.3)
+                .frame(width: 32, height: 32)
             
-            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, autheticationError in
-                DispatchQueue.main.async {
-                    if success {
-                        self.isUnlock = true
-                    } else {
-                        // there was a problem
+            VStack {
+                Spacer()
+                HStack {
+                    Spacer()
+                    Button(action: {
+                        let newLocation = MKPointAnnotation()
+                        newLocation.coordinate = self.centerCoordinate
+                        self.locations.append(newLocation)
+                    }) {
+                        Image(systemName: "plus")
                     }
+                    .padding()
+                    .background(Color.black.opacity(0.75))
+                    .foregroundColor(.white)
+                    .font(.title)
+                    .clipShape(Circle())
+                    .padding(.trailing)
                 }
             }
-        } else {
-            // no biometrics
         }
     }
     
