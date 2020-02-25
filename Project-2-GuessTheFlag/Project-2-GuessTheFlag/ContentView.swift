c4a30f0ffdc6776aee401c2f14b14b09fbc25be0
Jonathan Miranda <miranda.jth@gmail.com>
2020-02-24 20:42:32 -0600

    creating project 2 Guess The Flag at this time to accomplish a step in project 15

---
+++ Project-2-GuessTheFlag/Project-2-GuessTheFlag/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project-2-GuessTheFlag
+//
+//  Created by Jonathan Miranda on 2/24/20.
+//  Copyright © 2020 Jonathan Miranda. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
