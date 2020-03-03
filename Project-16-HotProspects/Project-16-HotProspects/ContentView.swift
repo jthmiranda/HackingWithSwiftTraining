d5285a673dc7312c61c3c9b32b1d8cad14687489
Jonathan Miranda <miranda.jth@gmail.com>
2020-03-02 22:16:40 -0600

    reading custom values from the environment with @EnvironmentObject

--- Project-16-HotProspects/Project-16-HotProspects/ContentView.swift
+++ Project-16-HotProspects/Project-16-HotProspects/ContentView.swift
@@ -9,10 +9,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    let user = User()
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            EditView()
+            DisplayView()
+        }
+        .environmentObject(user)
     }
 }
+
+struct EditView: View {
+    @EnvironmentObject var user: User
+    
+    var body: some View {
+        TextField("Name", text: $user.name)
+    }
+}
+
+struct DisplayView: View {
+    @EnvironmentObject var user: User
+    
+    var body: some View {
+        Text(user.name)
+    }
+}
+
+class User: ObservableObject {
+    @Published var name = "Taylor Swift"
+}
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
