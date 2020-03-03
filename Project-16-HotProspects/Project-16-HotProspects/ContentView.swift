26c2495586c992ab080bd54c09e5517832e864a4
Jonathan Miranda <miranda.jth@gmail.com>
2020-03-02 22:29:57 -0600

    creating tabs with TabView and tabItem()

--- Project-16-HotProspects/Project-16-HotProspects/ContentView.swift
+++ Project-16-HotProspects/Project-16-HotProspects/ContentView.swift
@@ -9,38 +9,29 @@
 import SwiftUI
 
 struct ContentView: View {
-    let user = User()
+    @State private var selectedTab = 0
+    
     
     var body: some View {
-        VStack {
-            EditView()
-            DisplayView()
+        TabView(selection: $selectedTab) {
+            Text("Tab 1")
+                .onTapGesture {
+                    self.selectedTab = 1
+                }
+                .tabItem {
+                    Image(systemName: "star")
+                    Text("One")
+                }
+                .tag(0)
+            Text("Tab 2")
+                .tabItem {
+                    Image(systemName: "star.fill")
+                    Text("Two")
+                }
+                .tag(1)
         }
-        .environmentObject(user)
     }
 }
-
-struct EditView: View {
-    @EnvironmentObject var user: User
-    
-    var body: some View {
-        TextField("Name", text: $user.name)
-    }
-}
-
-struct DisplayView: View {
-    @EnvironmentObject var user: User
-    
-    var body: some View {
-        Text(user.name)
-    }
-}
-
-class User: ObservableObject {
-    @Published var name = "Taylor Swift"
-}
-
-
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
