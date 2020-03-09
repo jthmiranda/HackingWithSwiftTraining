7867fc68eaff06eb5a7b9f49c228302a92de3ceb
Jonathan Miranda <miranda.jth@gmail.com>
2020-03-09 16:05:36 -0600

    Disabling user interactivity with allowsHitTesting()

--- Project-17-Flashzilla/Project-17-Flashzilla/ContentView.swift
+++ Project-17-Flashzilla/Project-17-Flashzilla/ContentView.swift
@@ -7,52 +7,40 @@
 //
 
 import SwiftUI
-import CoreHaptics
 
 struct ContentView: View {
-    @State private var engine: CHHapticEngine?
+    
     
     var body: some View {
-        Text("Hello, World!")
-            .onAppear(perform: prepareHaptics)
-            .onTapGesture(perform: complexSucess)
-    }
-    
-    func simpleSuccess() {
-        let generator = UINotificationFeedbackGenerator()
-        generator.notificationOccurred(.warning)
-    }
-    
-    
-    func prepareHaptics() {
-        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
-        
-        do {
-            self.engine = try CHHapticEngine()
-            try engine?.start()
-        } catch {
-            print("There was a error creating the engine: \(error.localizedDescription)")
+        VStack {
+            VStack {
+                Text("Hello")
+                // SwiftUI won't trigger actions when a stack spacer is tapped
+                Spacer().frame(height: 100)
+                
+                Text("World")
+            }
+            .onTapGesture {
+                print("Vstack tapped!")
+            }
+            
+            Spacer().frame(height: 100)
+            
+            VStack {
+                Text("Hello")
+                
+                Spacer().frame(height: 100)
+                
+                Text("World")
+            }
+                // If you want the whole vstack tappalge use contentShape modifier
+                .contentShape(Rectangle())
+                .onTapGesture {
+                    print("Vstack tapped!")
+            }
         }
     }
     
-    func complexSucess() {
-        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
-        var events = [CHHapticEvent]()
-        
-        let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: 1)
-        let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: 1)
-        let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: 0)
-        
-        events.append(event)
-        
-        do {
-            let pattern = try CHHapticPattern(events: events, parameters: [])
-            let player = try engine?.makePlayer(with: pattern)
-            try player?.start(atTime: 0)
-        } catch {
-            print("Failed to play pattern: \(error.localizedDescription)")
-        }
-    }
 }
 
 struct ContentView_Previews: PreviewProvider {
