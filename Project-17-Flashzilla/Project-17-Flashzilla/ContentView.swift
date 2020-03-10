bb58f019f9f215eb08a625d705cabef93e8e18e9
Jonathan Miranda <miranda.jth@gmail.com>
2020-03-10 16:41:02 -0600

    support specific accessibility needs with SwiftUI

--- Project-17-Flashzilla/Project-17-Flashzilla/ContentView.swift
+++ Project-17-Flashzilla/Project-17-Flashzilla/ContentView.swift
@@ -9,29 +9,55 @@
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.accessibilityDifferentiateWithoutColor) var differentiateWithoutColor
+    @Environment(\.accessibilityReduceMotion) var reduceMotion
+    @Environment(\.accessibilityReduceTransparency) var reduceTransparency
     
+    @State private var scale: CGFloat = 1
     
     var body: some View {
         VStack {
-            Text("Going background")
-                .onReceive(NotificationCenter.default.publisher(for: UIApplication.willResignActiveNotification)) { _ in
-                    print("Moving to the background")
+            HStack {
+                if differentiateWithoutColor {
+                    Image(systemName: "checkmark.circle")
                 }
-                .onReceive(NotificationCenter.default.publisher(for: UIApplication.willEnterForegroundNotification)) { _ in
-                    print("Moving back to the foreground!")
+                
+                Text("Success")
+            }
+            .padding()
+            .background(differentiateWithoutColor ? Color.black : Color.green)
+            .foregroundColor(Color.white)
+            .clipShape(Capsule())
+            
+            Text("Hello World")
+            .scaleEffect(scale)
+            .onTapGesture {
+                self.withOptionalAnimation {
+                    self.scale *= 1.5
                 }
-                .onReceive(NotificationCenter.default.publisher(for: UIApplication.userDidTakeScreenshotNotification)) { _ in
-                    print("User took a screenshot!")
-                }
-                .onReceive(NotificationCenter.default.publisher(for: UIApplication.significantTimeChangeNotification)) { _ in
-                    print("User changes clock or daylight savings")
-                }
-                .onReceive(NotificationCenter.default.publisher(for: UIApplication.keyboardDidShowNotification)) { _ in
-                    print("Keyboars is show")
-                }
+//                if self.reduceMotion {
+//                    self.scale *= 1.5
+//                } else {
+//                    withAnimation {
+//                        self.scale *= 1.5
+//                    }
+//                }
+            }
+            
+            Text("Hello Transparency")
+                .padding()
+                .background(reduceTransparency ? Color.black : Color.black.opacity(0.5))
+                .foregroundColor(Color.white)
+                .clipShape(Capsule())
         }
-        
-        
+    }
+    
+    func withOptionalAnimation<Result>(_ animation: Animation? = .default, _ body: () throws -> Result) rethrows -> Result {
+        if UIAccessibility.isReduceMotionEnabled {
+            return try body()
+        } else {
+            return try withAnimation(animation, body)
+        }
     }
     
 }
