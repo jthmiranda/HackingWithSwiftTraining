41eba11283bda1d05ea48c15fedd946e3022a292
Jonathan Miranda <miranda.jth@gmail.com>
2020-03-13 10:04:07 -0600

    ending the app with allowTesting()

--- Project-17-Flashzilla/Project-17-Flashzilla/ContentView.swift
+++ Project-17-Flashzilla/Project-17-Flashzilla/ContentView.swift
@@ -12,6 +12,11 @@ struct ContentView: View {
     @State private var cards = [Card](repeating: Card.example, count: 10)
     @Environment(\.accessibilityDifferentiateWithoutColor) var differentiateWithoutColor
     
+    @State private var timeRemaining = 100
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
+    @State private var isActive = true
+    
     var body: some View {
         ZStack {
             Image("background")
@@ -20,6 +25,16 @@ struct ContentView: View {
                 .edgesIgnoringSafeArea(.all)
             
             VStack {
+                Text("Time: \(timeRemaining)")
+                    .font(.largeTitle)
+                    .foregroundColor(.white)
+                    .padding(.horizontal, 20)
+                    .padding(.vertical, 5)
+                    .background(
+                        Capsule()
+                            .fill(Color.black)
+                            .opacity(0.75)
+                    )
                 if differentiateWithoutColor {
                     VStack {
                         Spacer()
@@ -41,6 +56,8 @@ struct ContentView: View {
                     }
                 }
                 
+                
+                
                 ZStack {
                     ForEach(0..<cards.count, id: \.self) { index in
                         CardView(card: self.cards[index]) {
@@ -51,13 +68,47 @@ struct ContentView: View {
                         .staked(at: index, in: self.cards.count)
                     }
                 }
+                .allowsHitTesting(timeRemaining > 0)
+                
+                // this will enable a button when all card have been removed
+                if cards.isEmpty {
+                    Button("Start Again", action: resetCards)
+                        .padding()
+                        .background(Color.white)
+                        .foregroundColor(.black)
+                        .clipShape(Capsule())
+                }
+            }
+        }
+        .onReceive(timer) { time in
+            guard self.isActive else { return }
+            if self.timeRemaining > 0 {
+                self.timeRemaining -= 1
+            }
+        }
+        .onReceive(NotificationCenter.default.publisher(for: UIApplication.willResignActiveNotification)) { _ in
+            self.isActive = false
+        }
+        .onReceive(NotificationCenter.default.publisher(for: UIApplication.willEnterForegroundNotification)) { _ in
+            if self.cards.isEmpty == false {
+                self.isActive = true
             }
         }
     }
     
-   
+    //this func will be effective by the invocation a closure inside CardView
     func removeCard(at index: Int) {
         cards.remove(at: index)
+        if cards.isEmpty {
+            isActive = false
+        }
+    }
+    
+    // func to restart again
+    func resetCards() {
+        cards = [Card](repeating: Card.example, count: 10)
+        timeRemaining = 100
+        isActive = true
     }
 }
 
