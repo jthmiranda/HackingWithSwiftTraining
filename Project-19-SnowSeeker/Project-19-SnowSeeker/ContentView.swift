63acfc080b751a5b381d85b4083b777f2d0dafb4
Jonathan Miranda <miranda.jth@gmail.com>
2020-03-16 15:53:12 -0600

    creating new project SnowSeeker #19

---
+++ Project-19-SnowSeeker/Project-19-SnowSeeker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project-19-SnowSeeker
+//
+//  Created by Jonathan Miranda on 3/16/20.
+//  Copyright © 2020 Jonathan Miranda. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
