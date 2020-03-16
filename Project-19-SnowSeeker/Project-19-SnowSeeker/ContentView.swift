4088e72f1e028a0115d0a12e69ce711d4afb4db2
Jonathan Miranda <miranda.jth@gmail.com>
2020-03-16 16:05:34 -0600

    Using alert() and sheet() with optionals

--- Project-19-SnowSeeker/Project-19-SnowSeeker/ContentView.swift
+++ Project-19-SnowSeeker/Project-19-SnowSeeker/ContentView.swift
@@ -8,13 +8,22 @@
 
 import SwiftUI
 
+struct User: Identifiable {
+    var id = "Taylor Swift"
+}
+
 struct ContentView: View {
+    @State private var selectedUser: User? = nil
+    @State private var isShowingAlert =  false
+    
     var body: some View {
-        NavigationView {
-             Text("Hello, World!")
-                .navigationBarTitle("Primary")
-            
-            Text("Secondary")
+        Text("Hello, World!")
+            .onTapGesture {
+                self.selectedUser = User()
+                self.isShowingAlert = true
+            }
+        .alert(isPresented: $isShowingAlert) { // prefer using the item's parameter
+            Alert(title: Text(selectedUser!.id)) // avoid this unwrap
         }
     }
 }
