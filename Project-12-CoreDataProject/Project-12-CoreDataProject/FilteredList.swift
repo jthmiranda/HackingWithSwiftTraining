5c1e54bebf318a38badca7944591a579ac43d05d
Jonathan Miranda <miranda.jth@gmail.com>
2020-04-06 20:51:25 -0600

    control predicate from parameter string

--- Project-12-CoreDataProject/Project-12-CoreDataProject/FilteredList.swift
+++ Project-12-CoreDataProject/Project-12-CoreDataProject/FilteredList.swift
@@ -20,8 +20,8 @@ struct FilteredList<T: NSManagedObject, Content: View>: View {
         }
     }
     
-    init(filterKey: String, filterValue: String, sortDescriptors: [NSSortDescriptor] = [], @ViewBuilder content: @escaping (T) -> Content ) {
-        fetchRequest = FetchRequest<T>(entity: T.entity(), sortDescriptors: sortDescriptors, predicate: NSPredicate(format: "%K BEGINSWITH %@", filterKey, filterValue))
+    init(filterKey: String, filterValue: String, sortDescriptors: [NSSortDescriptor] = [], predicate: String = "BEGINSWITH", @ViewBuilder content: @escaping (T) -> Content ) {
+        fetchRequest = FetchRequest<T>(entity: T.entity(), sortDescriptors: sortDescriptors, predicate: NSPredicate(format: "%K \(predicate) %@", filterKey, filterValue))
         self.content = content
     }
 }
