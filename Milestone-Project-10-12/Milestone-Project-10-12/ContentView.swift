4ef187fd9a69f0a6b3db0466f70031451edda13c
Jonathan Miranda <miranda.jth@gmail.com>
2020-04-08 20:50:18 -0600

    creating the milestone project for days 60-61

---
+++ Milestone-Project-10-12/Milestone-Project-10-12/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Milestone-Project-10-12
+//
+//  Created by Jonathan Miranda on 4/8/20.
+//  Copyright © 2020 Jonathan Miranda. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
