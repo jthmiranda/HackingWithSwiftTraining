f6bc3f24e87348788bd8904237f6da522aa4d92a
Jonathan Miranda <miranda.jth@gmail.com>
2020-05-27 23:53:46 -0600

    Starting project 11 Bookworm

---
+++ Project-11-Bookworm/Project-11-Bookworm/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project-11-Bookworm
+//
+//  Created by Jonathan Miranda on 5/27/20.
+//  Copyright © 2020 Jonathan Miranda. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
