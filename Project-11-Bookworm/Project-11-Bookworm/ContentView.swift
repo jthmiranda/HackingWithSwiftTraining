c3133904f80cd16cd66b10695be28970cb1fdef7
Jonathan Miranda <miranda.jth@gmail.com>
2020-05-31 21:48:21 -0600

    removing and replace data for going to the next exercise

--- Project-11-Bookworm/Project-11-Bookworm/ContentView.swift
+++ Project-11-Bookworm/Project-11-Bookworm/ContentView.swift
@@ -5,42 +5,14 @@
 //  Created by Jonathan Miranda on 5/27/20.
 //  Copyright © 2020 Jonathan Miranda. All rights reserved.
 //
-import CoreData
+
 import SwiftUI
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) var moc
-    @State private var lastNameFilter = "A"
     
     var body: some View {
         VStack {
-            FilteredList(filterKey: "lastName", filterValue: lastNameFilter) { (singer: Singer) in
-                Text("\(singer.wrappedFirstName) \(singer.wrappedLastName)")
-            }
-            
-            Button("Add Examples") {
-                let taylor = Singer(context: self.moc)
-                taylor.firstName = "Taylor"
-                taylor.lastName = "Swift"
-
-                let ed = Singer(context: self.moc)
-                ed.firstName = "Ed"
-                ed.lastName = "Sheeran"
-
-                let adele = Singer(context: self.moc)
-                adele.firstName = "Adele"
-                adele.lastName = "Adkins"
-                
-                try? self.moc.save()
-            }
-            
-            Button("Show A") {
-                self.lastNameFilter = "A"
-            }
-            
-            Button("Show S") {
-                self.lastNameFilter = "S"
-            }
+            Text("example")
         }
     }
 }
