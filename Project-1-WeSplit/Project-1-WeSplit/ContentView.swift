5fc0374251dd3d86d2fd46cc4b3527b58fe2f166
Jonathan Miranda <miranda.jth@gmail.com>
2020-06-12 20:27:34 -0600

    finishing WeSplit project 1

--- Project-1-WeSplit/Project-1-WeSplit/ContentView.swift
+++ Project-1-WeSplit/Project-1-WeSplit/ContentView.swift
@@ -9,8 +9,53 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmount = ""
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 2
+    
+    let tipPercentages = [10, 15 , 20, 25, 0]
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentages[tipPercentage])
+        let ordenAmount = Double(checkAmount) ?? 0
+        
+        let tipValue = ordenAmount / 100 * tipSelection
+        let granTotal = ordenAmount + tipValue
+        let amountPerPerson = granTotal / peopleCount
+        
+        return amountPerPerson
+    }
+    
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", text: $checkAmount)
+                        .keyboardType(.decimalPad)
+                    
+                    Picker("Nuber of People", selection: $numberOfPeople) {
+                        ForEach(2..<100) {
+                            Text("\($0) People")
+                        }
+                    }
+                }
+                
+                Section(header: Text("How much tip do you want to leave")) {
+                    Picker("Tip Percentage", selection: $tipPercentage) {
+                        ForEach(0..<self.tipPercentages.count) {
+                            Text("\(self.tipPercentages[$0])")
+                        }
+                    }
+                .pickerStyle(SegmentedPickerStyle())
+                }
+                
+                Section {
+                    Text("$\(totalPerPerson, specifier: "%.2f")")
+                }
+            }
+        .navigationBarTitle("WeSplit")
+        }
     }
 }
 
