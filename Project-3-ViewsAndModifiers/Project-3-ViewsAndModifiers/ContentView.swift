809397aa0d23f8df9976afd30a0c54dba5b8c2e1
Jonathan Miranda <miranda.jth@gmail.com>
2020-06-17 19:06:49 -0600

    Custom Containers

--- Project-3-ViewsAndModifiers/Project-3-ViewsAndModifiers/ContentView.swift
+++ Project-3-ViewsAndModifiers/Project-3-ViewsAndModifiers/ContentView.swift
@@ -8,33 +8,38 @@
 
 import SwiftUI
 
-struct WaterMark: ViewModifier {
-    var text: String
+struct GridStack<Content: View>: View {
+    let rows: Int
+    let columns: Int
+    let content: (Int, Int) -> Content
     
-    func body(content: Content) -> some View {
-        ZStack(alignment: .bottomTrailing) {
-            content
-            Text(text)
-                .font(.caption)
-                .foregroundColor(.white)
-                .padding()
-                .background(Color.black)
+    var body: some View {
+        VStack {
+            ForEach(0..<rows, id: \.self) { row in
+                HStack {
+                    ForEach(0..<self.columns, id: \.self) { column in
+                        self.content(row, column)
+                    }
+                }
+            }
         }
     }
-}
-
-extension View {
-    func watermarked(with text: String) -> some View {
-        self.modifier(WaterMark(text: text))
+    
+    init(rows: Int, columns: Int, @ViewBuilder content: @escaping (Int, Int) -> Content) {
+        self.rows = rows
+        self.columns = columns
+        self.content = content
     }
 }
 
 struct ContentView: View {
     
     var body: some View {
-        Color.blue
-            .frame(width: 300, height: 200)
-            .watermarked(with: "Hacking with Swift")
+        GridStack(rows: 4, columns: 4) { (row, col) in
+            Image(systemName: "\(row * 4 + col).circle")
+            Text("R\(row) C\(col)")
+        }
+            
     }
 }
 
