dc1198e8428ec7c774027af194c191a8fe2100ae
Jonathan Miranda <miranda.jth@gmail.com>
2020-06-23 22:01:13 -0600

    building a basic layout

--- Project-4-BetterRest/Project-4-BetterRest/ContentView.swift
+++ Project-4-BetterRest/Project-4-BetterRest/ContentView.swift
@@ -10,14 +10,50 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var wakeUp = Date()
+    @State private var sleepAmount = 8.0
+    @State private var coffeAmount = 1
     
     var body: some View {
-        let formatter = DateFormatter()
-        formatter.timeStyle = .short
-        let dateString = formatter.string(from: Date())
+        NavigationView {
+            VStack {
+                Text("When do you want to wake up?")
+                    .font(.headline)
+                
+                DatePicker("Please enter a time",
+                           selection: $wakeUp,
+                           displayedComponents:
+                            .hourAndMinute)
+                    .labelsHidden()
+                
+                Text("Desired amoount of sleep")
+                    .font(.headline)
+                
+                Stepper(value: $sleepAmount, in: 4...12, step: 0.25) {
+                    Text("\(sleepAmount, specifier: "%g") hours")
+                }
+                
+                Text("Daily coffee intake")
+                    .font(.headline)
+                
+                Stepper(value: $coffeAmount, in: 1...20) {
+                    if coffeAmount == 1 {
+                        Text("1 cup")
+                    } else {
+                        Text("\(coffeAmount) cups")
+                    }
+                }
+            }
+            .navigationBarTitle("BetterRest")
+            .navigationBarItems(trailing:
+                Button(action: calculateBedtime) {
+                    Text("Calculate")
+                }
+            )
+        }
+    }
+    
+    func calculateBedtime() {
         
-        return DatePicker("", selection: $wakeUp, in: Date()..., displayedComponents: .date)
-            .labelsHidden()
     }
 }
 
