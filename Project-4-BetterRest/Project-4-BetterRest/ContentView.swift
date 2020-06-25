3d95508f8e689ba84cc5aa9127ea355449748874
Jonathan Miranda <miranda.jth@gmail.com>
2020-06-24 21:20:59 -0600

    Connecting SwiftUI to Core ML

--- Project-4-BetterRest/Project-4-BetterRest/ContentView.swift
+++ Project-4-BetterRest/Project-4-BetterRest/ContentView.swift
@@ -13,6 +13,10 @@ struct ContentView: View {
     @State private var sleepAmount = 8.0
     @State private var coffeAmount = 1
     
+    @State private var alerTitle = ""
+    @State private var alertMessage = ""
+    @State private var showingAlert = false
+    
     var body: some View {
         NavigationView {
             VStack {
@@ -49,11 +53,31 @@ struct ContentView: View {
                     Text("Calculate")
                 }
             )
+            .alert(isPresented: $showingAlert) {
+                Alert(title: Text(alerTitle), message: Text(alertMessage), dismissButton: .default(Text("OK")))
+            }
         }
     }
     
     func calculateBedtime() {
-        
+        let model = SleepCalculator()
+        let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+        let hour = (components.hour ?? 0) * 60 * 60
+        let minute = (components.minute ?? 0) * 60
+        do {
+            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeAmount))
+            let sleepTime = wakeUp - prediction.actualSleep
+            
+            let formatter = DateFormatter()
+            formatter.timeStyle = .short
+            
+            alertMessage = formatter.string(from: sleepTime)
+            alerTitle = "Your idel bedtime is..."
+        } catch {
+            alerTitle = "Error"
+            alertMessage = "Sorry, there was a problem calculating your bedtime"
+        }
+        showingAlert = true
     }
 }
 
