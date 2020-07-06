ada143c4a32f62c4d8451b17134af0ccae406ceb
Jonathan Miranda <miranda.jth@gmail.com>
2020-07-05 22:02:18 -0600

    adding project 7 iExpense

---
+++ Project-7-iExpense/Project-7-iExpense/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project-7-iExpense
+//
+//  Created by Jonathan Miranda on 7/5/20.
+//  Copyright © 2020 Jonathan Miranda. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
