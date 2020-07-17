1f7223c1387b254d69485701684aadc577b59ac0
Jonathan Miranda <miranda.jth@gmail.com>
2020-07-16 21:13:53 -0600

    Fixing problems with buttonStyle() and layoutPriority()

---
+++ Project-8-Moonshot/Project-8-Moonshot/AstronautView.swift
@@ -0,0 +1,38 @@
+//
+//  AstronautView.swift
+//  Project-8-Moonshot
+//
+//  Created by Jonathan Miranda on 7/16/20.
+//  Copyright © 2020 Jonathan Miranda. All rights reserved.
+//
+
+import SwiftUI
+
+struct AstronautView: View {
+    let astronaut: Astronaut
+    
+    var body: some View {
+        GeometryReader { geometry in
+            ScrollView(.vertical) {
+                VStack {
+                    Image(self.astronaut.id)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: geometry.size.width)
+                    
+                    Text(self.astronaut.name)
+                        .padding()
+                }
+            }
+        }
+        .navigationBarTitle(Text(astronaut.name), displayMode: .inline)
+    }
+}
+
+struct AstronautView_Previews: PreviewProvider {
+    static let astronauts: [Astronaut] = Bundle.main.decode("astronauts.json")
+    
+    static var previews: some View {
+        AstronautView(astronaut: astronauts[0])
+    }
+}
