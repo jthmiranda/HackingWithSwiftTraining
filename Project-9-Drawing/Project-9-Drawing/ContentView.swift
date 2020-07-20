be3e9f0f5fc9ffd48aae07c6106bd9e6b0297820
Jonathan Miranda <miranda.jth@gmail.com>
2020-07-19 20:22:02 -0600

    creating custom paths with SwiftIU

--- Project-9-Drawing/Project-9-Drawing/ContentView.swift
+++ Project-9-Drawing/Project-9-Drawing/ContentView.swift
@@ -10,10 +10,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        Path { path in
+            path.move(to: CGPoint(x: 200, y: 100))
+            path.addLine(to: CGPoint(x: 100, y: 300))
+            path.addLine(to: CGPoint(x: 300, y: 300))
+            path.addLine(to: CGPoint(x: 200, y: 100))
+            path.addLine(to: CGPoint(x: 100, y: 300))
+        }
+        
+        .stroke(Color.blue.opacity(0.25), lineWidth: 10)
     }
 }
-
+ 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
