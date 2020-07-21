4414a1082041cc9da2636c9420a29795eb27c8ea
Jonathan Miranda <miranda.jth@gmail.com>
2020-07-21 16:27:13 -0600

    Enabling high-performance Metal rendering with drawingGroup()

--- Project-9-Drawing/Project-9-Drawing/ContentView.swift
+++ Project-9-Drawing/Project-9-Drawing/ContentView.swift
@@ -8,43 +8,44 @@
 
 import SwiftUI
 
-struct Flower: Shape {
-    var petalOffsets: Double = -20
-    var petalWidth: Double = 100
+struct ColorCyclingView: View {
+    var amount = 0.0
+    var steps = 100
     
-    func path(in rect: CGRect) -> Path {
-        var path = Path()
+    var body: some View {
+        ZStack {
+            ForEach(0..<steps) { value in
+                Circle()
+                    .inset(by: CGFloat(value))
+                    .strokeBorder(LinearGradient(gradient: Gradient(colors: [
+                        self.color(for: value, brigthness: 1),
+                        self.color(for: value, brigthness: 0.5)
+                    ]), startPoint: .top, endPoint: .bottom), lineWidth: 2)
+            }
+        }
+        .drawingGroup()
+    }
+    
+    func color(for value: Int, brigthness: Double) -> Color {
+        var targetHue = Double(value) / Double(self.steps) + self.amount
         
-        for number in stride(from: 0, to: CGFloat.pi * 2, by: CGFloat.pi / 8) {
-            let rotation = CGAffineTransform(rotationAngle: number)
-            let position = rotation.concatenating(CGAffineTransform(translationX: rect.width / 2, y: rect.height / 2))
-            let originalPetal = Path(ellipseIn: CGRect(x: CGFloat(petalOffsets), y: 0, width: CGFloat(petalWidth), height: rect.width / 2))
-            let rotatedPedal = originalPetal.applying(position)
-            
-            path.addPath(rotatedPedal)
+        if targetHue > 1 {
+            targetHue -= 1
         }
         
-        return path
+        return Color(hue: targetHue, saturation: 1, brightness: brigthness)
     }
 }
 
-
 struct ContentView: View {
-    @State private var petalOffset = -20.0
-    @State private var petalWidth = 100.0
+    @State private var colorCycle = 0.0
     
     var body: some View {
         VStack {
-            Flower(petalOffsets: petalOffset, petalWidth: petalWidth)
-                .fill(Color.red, style: FillStyle(eoFill: true))
+            ColorCyclingView(amount: self.colorCycle)
+                .frame(width: 300, height: 300)
             
-            Text("Offset")
-            Slider(value: $petalOffset, in: -40...40)
-                .padding([.horizontal, .bottom])
-            
-            Text("Width")
-            Slider(value: $petalWidth, in: 1...100)
-                .padding(.horizontal)
+            Slider(value: $colorCycle)
         }
     }
 }
